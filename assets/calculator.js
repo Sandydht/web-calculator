8bc9f117734cbdd5acb835953333e437ff115fba
Sandydht <sandydwi75@gmail.com>
2021-09-08 21:30:17 +0700

    Create a "performCalculation" function.

--- assets/calculator.js
+++ assets/calculator.js
@@ -42,6 +42,22 @@ function handleOperator(operator) {
     alert('Operator sudah ditetapkan');
   }
 };
+
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert('Anda belum menetapkan operator');
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === '+') {
+    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+
+  calculator.displayNumber = result;
+};
  
 const buttons = document.querySelectorAll('.button');
 
